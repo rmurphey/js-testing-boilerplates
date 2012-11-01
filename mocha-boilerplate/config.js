6227a22087d96d9001f78775ba2b8dcf3bfed96d
Rebecca Murphey <rmurphey@gmail.com>
2012-11-01 16:19:25 -0400

    adding vendor files and updating config

--- mocha-boilerplate/config.js
+++ mocha-boilerplate/config.js
@@ -4,15 +4,16 @@
 var mode = 'tdd';
 
 // location of your app files relative to this file
-var appDir = '../fake-app-dir';
+var appDir = '../fake-app-dir/js';
 
 // location of all of the app files you'd like to test,
 // relative to the appDir specified above. do not include
-// the file extension. for example:
+// the file extension.
+//
+// for example:
 //
 //    var appFiles = [
-//      'main',
-//      'user'
+//      'app'
 //    ];
 //
 // alternately, if you understand how to use the shim object
@@ -23,29 +24,43 @@ var appFiles = [
 ];
 
 // any vendor libraries or plugins you are using, and their
-// location relative to this configuration file
+// location relative to this configuration file.
+//
+// for example:
+//
+//    var appLibs = {
+//      backbone: '../fake-app-dir/vendor/backbone-min',
+//      underscore: '../fake-app-dir/vendor/underscore-min',
+//      jquery: '../fake-app-dir/vendor/jquery'
+//    };
 var appLibs = {
-  backbone: '../vendor/backbone',
-  underscore: '../vendor/underscore',
-  jquery: '../vendor/jquery'
+
 };
 
 // if your vendor libraries have dependencies on other libraries,
 // or if your vendor libraries provide a global variable, use this
 // object to indicate them.
+//
+// for example:
+//
+//    var shim = {
+//      backbone: {
+//        deps: [ 'jquery', 'underscore' ],
+//        exports: 'Backbone'
+//      },
+//      underscore: {
+//        exports: '_'
+//      }
+//    };
+
 var shim = {
-  backbone: {
-    deps: [ 'jquery', 'underscore' ],
-    exports: 'Backbone'
-  },
-  underscore: {
-    exports: '_'
-  }
+
 };
 
 
 
-// modifying stuff below here is expert mode :)
+// you shouldn't need to modify anything below here, and if you do
+// modify anything below here, you should know what you're doing :)
 
 if ( mode === 'tdd' ) {
   window.assert = chai.assert;
@@ -54,6 +69,8 @@ if ( mode === 'tdd' ) {
 if ( mode === 'bdd' ) {
   window.should = chai.should();
 }
+
+mocha.setup( mode );
 
 var paths = {
   app: appDir
