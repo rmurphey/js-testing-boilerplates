849822b75ca6622418db20a6cfc1ccaaa4552730
Rebecca Murphey <rmurphey@gmail.com>
2012-11-04 20:00:22 -0500

    adding mocha info to grunt

--- mocha-boilerplate/grunt.js
+++ mocha-boilerplate/grunt.js
@@ -25,6 +25,10 @@ module.exports = function(grunt) {
         browser: true
       },
       globals: {}
+    },
+    mocha: {
+      index: [ 'runner/index.html' ],
+      run: true
     }
   });
 
